9adf002253b4698b5384af585bb33bddb141bd66
JieYin <jyin19982021@gmail.com>
2021-12-01 00:53:08 -0500

    add rigiter and login layout

--- app/src/main/java/com/example/app2/account.java
+++ app/src/main/java/com/example/app2/account.java
@@ -13,8 +13,12 @@ public class account extends AppCompatActivity {
         setContentView(R.layout.account);
         Button button;
         button=findViewById(R.id.button);
+        Button button3;
+        button3=findViewById(R.id.button3);
         Button button8;
         button8=findViewById(R.id.button8);
+        Button button4;
+        button4=findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -22,10 +26,24 @@ public class account extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(account.this,forget.class);
+                startActivity(intent);
+            }
+        });
         button8.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(account.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(account.this,femail.class);
                 startActivity(intent);
             }
         });
