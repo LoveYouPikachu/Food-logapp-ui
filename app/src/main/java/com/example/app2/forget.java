636a02f57da10e7a7f0e3cbd7af4ca2d7ee1b341
JieYin <jyin19982021@gmail.com>
2021-12-09 13:37:52 -0500

    add forget function

---
+++ app/src/main/java/com/example/app2/forget.java
@@ -0,0 +1,16 @@
+package com.example.app2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class forget extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget);
+
+    }
+}
